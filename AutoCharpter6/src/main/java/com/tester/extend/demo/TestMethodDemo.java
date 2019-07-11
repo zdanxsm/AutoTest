c4c302ba7f792a18467a19bb01b67edfb08fb3f7
zdanxsm <ouyangweihong189@163.com>
2019-07-11 16:12:00 +0800

    add test report

---
+++ AutoCharpter6/src/main/java/com/tester/extend/demo/TestMethodDemo.java
@@ -0,0 +1,30 @@
+package com.tester.extend.demo;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestMethodDemo {
+
+    @Test
+    public void test1(){
+        Assert.assertEquals(1,2);
+
+    }
+
+    @Test
+    public void test2(){
+        Assert.assertEquals(1,1);
+    }
+
+    @Test
+    public void test3(){
+        Assert.assertEquals("aaa","aaa");
+    }
+
+    @Test
+    public void logDemo(){
+        Reporter.log("this is log write by us");
+        throw new RuntimeException("this is 我自己的运行异常");
+    }
+}
